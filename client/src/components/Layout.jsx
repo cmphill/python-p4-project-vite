3efd5036adef57c8977c35f1edc367bc11cde408
Cooper <cmphill@pm.me>
2023-08-28 15:37:30 -0600

    added a layout file

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink, Outlet} from 'react-router-dom';
+import Search from './Search.jsx';
+
+function Layout {
+
+    return( 
+    
+    <div className = 'Layout'>
+        <header>
+        <div className="NavBar">
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="community"> Community </NavLink>
+            <NavLink to="personal"> Personal </NavLink>
+            <NavLink to="login"> Sign Up </NavLink>
+          </nav>
+          <Search onSearchMemorabilia={onSearchMemorabilia} />
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <p />
+    </div>
+    );
+}
+
+export default Layout;
