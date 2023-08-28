eb4d60b8d00a48bf05818690d3a93afbe385e610
Cooper <cmphill@pm.me>
2023-08-28 17:27:07 -0600

    added the signup form

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -78,38 +78,9 @@ export const SignupForm = () => {
         <label htmlFor="age">age</label>
         <br />
 
-        <input
-          id="age"
-          name="age"
-          onChange={formik.handleChange}
-          value={formik.values.age}
-        />
-        <p style={{ color: "red" }}> {formik.errors.age}</p>
-        <button type="submit">Submit</button>
-      </form>
-      <table style={{ padding: "15px" }}>
-        <tbody>
-          <tr>
-            <th>name</th>
-            <th>email</th>
-            <th>age</th>
-          </tr>
-          {customers === "undefined" ? (
-            <p>Loading</p>
-          ) : (
-            customers.map((customer, i) => (
-              <>
-                <tr key={i}>
-                  <td>{customer.name}</td>
-                  <td>{customer.email}</td>
-                  <td>{customer.age}</td>
-                </tr>
-              </>
-            ))
-          )}
-        </tbody>
-      </table>
-    </div>
-  );
+function SignUpForm ({onLogin}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [age, setAge]
 }
-export default SignupForm;
