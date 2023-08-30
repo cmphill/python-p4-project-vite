63d6b372c9015c22a4ccf26799c08499428d7cc0
Cooper <cmphill@pm.me>
2023-08-30 09:27:30 -0600

    fixed the forms

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -201,9 +201,7 @@ function SignUpForm() {
           >
             {isSubmitting ? "Loading" : "Sign up"}
           </button>
-          {errors.map((error, index) => (
-            <p key={index}>{error}</p>
-          ))}
+         
         </Form>
       )}
     </Formik>
