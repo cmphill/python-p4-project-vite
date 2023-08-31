2da4ed81e35b09fcf62183be97f18602cfd9d867
Jerricke <eejerrickw@gmail.com>
2023-08-31 17:55:37 -0600

    added css for new comment form

--- client/src/components/CommunityComment.jsx
+++ client/src/components/CommunityComment.jsx
@@ -4,56 +4,58 @@ import * as yup from "yup";
 
 function CommunityComment({ addComment, user_id, user }) {
   
-  const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState({})
 
-  const formSchema = yup.object().shape({
-    comment: yup.string().max(500, 'maxium 500 characters').required("Must enter a comment"),
-  });
+    const formSchema = yup.object().shape({
+        comment: yup.string().max(500, 'maxium 500 characters').required("Must enter a comment"),
+    });
 
-  const formik = useFormik({
-    initialValues: {
-      comment: '',
-    },
-    validationSchema: formSchema,
-    onSubmit: (values) => {
-      fetch("/api/communitycomments", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    const formik = useFormik({
+        initialValues: {
+        comment: '',
         },
-        body: JSON.stringify({
-            values,
-            user_id: user_id
-        }),
-      }).then(res => {
-        if (res.ok) {
-            res.json().then(values => addComment(values))
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+        fetch("/api/communitycomments", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                values,
+                user_id: user_id
+            }),
+        }).then(res => {
+            if (res.ok) {
+                res.json().then(values => addComment(values))
+                }
+            else {
+            res.json().then(errors => {
+                console.log('we\'ve got errors')
+                setErrors(errors.message)
+                console.log(errors)
+            })
             }
-        else {
-         res.json().then(errors => {
-            console.log('we\'ve got errors')
-            setErrors(errors.message)
-            console.log(errors)
-         })
-        }
-    })
-        
-    },
-  });
+        })
+            
+        },
+    });
 
 
   return (
-    <div className="CommunityComment">
-    <form onSubmit={formik.handleSubmit}>
-        <div className="form-group">
-            <label htmlFor="comment">Comment</label>
-            <textarea style={{resize: "none"}}className="rounded border-b-2 border-black" type="text" rows="3" name="comment" placeholder="comment" value={formik.values.comment} onChange={formik.handleChange} />
-            {formik.errors.content && formik.touched.content && <div className="error">{formik.errors.content}</div>}
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
-</div>
-  );
+    <div className="community-comment-form">
+        <form onSubmit={formik.handleSubmit}>
+            <div className="form-group">
+                <label className="font-bold" htmlFor="comment">New Comment</label>
+                <br />
+                <textarea style={{resize: "none", width: "20vw"}} className="rounded border-2 border-black" type="text" rows="3" name="comment" placeholder="comment" value={formik.values.comment} onChange={formik.handleChange} />
+                {formik.errors.content && formik.touched.content && <div className="error">{formik.errors.content}</div>}
+                <br />
+                <button type="submit" className="btn border-2 px-5 rounded border-gray-400">Submit</button>
+            </div>
+        </form>
+    </div>
+    );
 }
 
 export default CommunityComment;
